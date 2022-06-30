627bc2252eb7c65a99583fe68a1712e5780a43cc
markFT39 <43339545+markFT39@users.noreply.github.com>
2022-06-30 15:41:54 +0900

    Day4 update

---
+++ HJ_STUDY/DAY_4/flutter_buttons/lib/main.dart
@@ -0,0 +1,121 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyButtons(),
+    );
+  }
+}
+
+class MyButtons extends StatelessWidget {
+  const MyButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Buttons'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                print('text button');
+              },
+              onLongPress: () {
+                print('Long text button');
+              },
+              child: Text(
+                'Text button',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              style: TextButton.styleFrom(
+                primary: Colors.red,
+                //backgroundColor: Colors.blue
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Elevated button');
+              },
+              child: Text('Elevated button'),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.orangeAccent,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  elevation: 0.0),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                print('Outlined button');
+              },
+              child: Text('Outlined button'),
+              style: OutlinedButton.styleFrom(
+                primary: Colors.green,
+                //side: BorderSide(color: Colors.black87, width: 2.0)
+              ),
+            ),
+            TextButton.icon(
+              onPressed: () {
+                print('Icon button');
+              },
+              icon: Icon(
+                Icons.home,
+                size: 30.0,
+                color: Colors.black87,
+              ),
+              label: Text('Go to the home'),
+              style: TextButton.styleFrom(primary: Colors.purple),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                print('Elevated button icon');
+              },
+              icon: Icon(Icons.home),
+              label: Text('Go to the home'),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.black, minimumSize: Size(200, 50)),
+            ),
+            OutlinedButton.icon(
+              onPressed: () {
+                print('Outlined button icon');
+              },
+              icon: Icon(Icons.home),
+              label: Text('Go to the home'),
+              style: OutlinedButton.styleFrom(
+                primary: Colors.black,
+              ),
+            ),
+            ElevatedButton.icon(
+              onPressed: null,
+              icon: Icon(Icons.home),
+              label: Text('Go to the home'),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.black, onSurface: Colors.pink),
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              buttonPadding: EdgeInsets.all(20),
+              children: [
+                TextButton(onPressed: () {}, child: Text('Text Button')),
+                ElevatedButton(
+                    onPressed: () {}, child: Text('Elevated Button')),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
