4ad0480d43d8f43ffa064fa45d66cd9c244d78e9
markFT39 <43339545+markFT39@users.noreply.github.com>
2022-07-04 12:35:40 +0900

    Day6 update1

---
+++ HJ_STUDY/DAY_6/chatting_app/lib/main.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:chatting_app/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginSignupScreen(),
+    );
+  }
+}
