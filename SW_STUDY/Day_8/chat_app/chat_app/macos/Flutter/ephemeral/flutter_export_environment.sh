db96e5aa0e7e57512750eccd6b687fa5455b911c
NotStoneSeok <98035984+isaccKim@users.noreply.github.com>
2022-07-07 13:03:50 +0900

    Updated Firebase connection

---
+++ SW_STUDY/Day_8/chat_app/chat_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/isacc_kim/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/isacc_kim/Desktop/chat_app/chat_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/isacc_kim/Desktop/chat_app/chat_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/isacc_kim/Desktop/chat_app/chat_app/.dart_tool/package_config.json"
