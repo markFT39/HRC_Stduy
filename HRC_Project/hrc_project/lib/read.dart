840fb24f445ef7a1bba138eb39d89bd9770a970f
NotStoneSeok <98035984+isaccKim@users.noreply.github.com>
2022-07-13 11:07:41 +0900

    First Commit

---
+++ HRC_Project/hrc_project/lib/read.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Name_def{
+  //user
+  double ?height;
+  double ?weight;
+  String ?username;
+  String ?email;
+  String ?pw;
+  double ?sum_distance;
+  double ?sum_time;
+  //running
+  double ?r_distance;
+  double ?r_time;
+  double ?r_pace;
+  double ?r_kacl;
+  double ?r_strength;
+  String ?r_date;
+  String ?r_title;
+}
