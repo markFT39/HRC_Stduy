b421b0d3d0d3586d31aa5c8cc5102755a5afd848
markFT39 <43339545+markFT39@users.noreply.github.com>
2022-07-18 13:51:54 +0900

    project sum

--- HJ_STUDY/hrc_project/lib/main.dart
+++ HJ_STUDY/hrc_project/lib/main.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'pages/start_page.dart';
+import 'login_page/pages/start_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- HJ_STUDY/ready made/login_page_test/android/app/src/main/kotlin/com/example/login_page_test/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.login_page_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
